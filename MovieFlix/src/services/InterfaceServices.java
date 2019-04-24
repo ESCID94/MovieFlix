cec7ee48965aff83f9a45106918e8b479ff1f905
NaderZaiter <zaayter99@gmail.com>
2019-04-24 12:37:16 +0200

    update services

---
+++ MovieFlix/src/services/InterfaceServices.java
@@ -0,0 +1,7 @@
+package services;
+
+public interface InterfaceServices  <T> {
+	public T add();
+	public T drop();
+	public T alter();
+}
---
+++ MovieFlix/src/services/Services.java
@@ -0,0 +1,22 @@
+package services;
+
+public class Services implements InterfaceServices {
+	InterfaceServices <?> datos = new Services();
+	@Override
+	public Object add() {
+		// TODO Auto-generated method stub
+		return datos.add();
+	}
+
+	@Override
+	public Object drop() {
+		// TODO Auto-generated method stub
+		return datos.drop();
+	}
+
+	@Override
+	public Object alter() {
+		// TODO Auto-generated method stub
+		 return datos.add();
+	}
+}
