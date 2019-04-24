c68a866ba25b8ca523f349ebbed7f9ea1e8df5c0
NaderZaiter <zaayter99@gmail.com>
2019-04-24 17:46:43 +0200

    añadir try catch DAOUser

--- MovieFlix/src/data/DAOUsuario.java
+++ MovieFlix/src/data/DAOUsuario.java
@@ -2,43 +2,55 @@ package data;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import connection.ConnectionBBDD;
 import model.User;
 
-public class DAOUsuario<T> implements IDAO<T>{
+public class DAOUsuario<T> implements IDAO<T> {
 
 	ConnectionBBDD connection = new ConnectionBBDD();
 	PreparedStatement miStatement;
-	
+
 	@Override
 	public void add(T t) throws SQLException {
-		// TODO Auto-generated method stub	
-	User u = (User) t; 
-	
-	miStatement = connection.getConnection().prepareStatement("INSERT INTO USERS VALUES(?,?,?,?,?)");
-	miStatement.setInt(1, u.getIdUser());
-	miStatement.setString(2, u.getNameUser());
-	miStatement.setString(3, u.getLastName());
-	miStatement.setString(4, u.getMail());
-	miStatement.setInt(5, u.getBirthYear());
-		
+		// TODO Auto-generated method stub
+		try {
+			User u = (User) t;
+			miStatement = connection.getConnection().prepareStatement("INSERT INTO USERS VALUES(?,?,?,?,?)");
+			miStatement.setInt(1, u.getIdUser());
+			miStatement.setString(2, u.getNameUser());
+			miStatement.setString(3, u.getLastName());
+			miStatement.setString(4, u.getMail());
+			miStatement.setInt(5, u.getBirthYear());
+		} catch (SQLException e) {
+			Logger lgr = Logger.getLogger(DAOUsuario.class.getName());
+			lgr.log(Level.SEVERE, e.getMessage(), e);
+		} finally {
+			try {
+				if (miStatement != null) {
+					miStatement.close();
+				}
+				if (connection != null) {
+					connection.getConnection().close();
+				}
+			} catch (SQLException ex) {
+				Logger lgr = Logger.getLogger(DAOUsuario.class.getName());
+				lgr.log(Level.SEVERE, ex.getMessage(), ex);
+			}
+		}
 	}
 
 	@Override
 	public void drop(T t) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void alter(T t) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-	
-	
-
 
 }
