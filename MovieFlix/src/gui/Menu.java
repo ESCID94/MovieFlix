1cfaa0a39ba9c3ae6054947a6c3371d206d21609
ESCID94 <kike15j@gmail.com>
2019-04-25 08:28:15 +0200

    update

---
+++ MovieFlix/src/gui/Menu.java
@@ -0,0 +1,27 @@
+package gui;
+
+public class Menu {
+	
+	public static void showMenu() {
+        System.out.println(" ");
+        System.out.println(" BIENVENIDO A MOVIEFLIX");
+        System.out.println(" ");
+        System.out.println("------------------------------------------------");
+        System.out.println("*  Por favor, escoge una opción: ");
+        System.out.println("*    1.-Dar de alta un usuario");
+        System.out.println("*    2.-Eliminar un usuario");
+        System.out.println("*    3.-Modificar un usuario");
+        System.out.println("*    4.-Dar de alta una película");
+        System.out.println("*    5.-Eliminar una película");
+        System.out.println("*    6.-Modificar una película");
+        System.out.println("*    7.-Dar de alta una categoría");
+        System.out.println("*    8.-Eliminar una categoría");
+        System.out.println("*    9.-Modificar una categoría");
+        System.out.println("*    0.-Salir");
+        System.out.println("------------------------------------------------");
+        System.out.println(" ");
+
+
+    }
+
+}
