6fad125cd9b1cf1c57c19173042c003701352cbd
ESCID94 <kike15j@gmail.com>
2019-04-26 13:00:02 +0200

    add Services

---
+++ MovieFlix/src/services/ServicesCatalog.java
@@ -0,0 +1,26 @@
+package services;
+
+import java.sql.SQLException;
+
+public class ServicesCatalog<T> implements IServices<T>{
+
+	@Override
+	public void add(T t) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drop(T t) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void alter(T t) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ MovieFlix/src/services/ServicesWatchlist.java
@@ -0,0 +1,25 @@
+package services;
+
+import java.sql.SQLException;
+
+public class ServicesWatchlist<T> implements IServices<T> {
+
+	@Override
+	public void add(T t) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drop(T t) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void alter(T t) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
